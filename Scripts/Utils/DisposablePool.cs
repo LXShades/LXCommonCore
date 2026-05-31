3cc6cffb05482f331c2948d0c2d76e71b9ac1fef
LXShades <louis_foy@yahoo.co.uk>
2026-05-31 17:02:53 +0100

    Fixing disposable pool rent bug

--- Scripts/Utils/DisposablePool.cs
+++ Scripts/Utils/DisposablePool.cs
@@ -25,7 +25,6 @@ namespace LX.Common.Core
 
         public static T RentNewInstance(T newInstance)
         {
-            instancesAvailableForRent.Add(newInstance);
             AddInstanceToPendingDisposal(newInstance);
             return newInstance;
         }
