8c3dd34fbe76d52031cae173b586ab0a29d6cdb2
LXShades <louis_foy@yahoo.co.uk>
2026-06-09 17:27:03 +0100

    Minor comment change

--- Scripts/Utils/KinematicMath.cs
+++ Scripts/Utils/KinematicMath.cs
@@ -5,7 +5,7 @@ namespace LX.Common.Core
     public static class KinematicMath
     {
         /// <summary>
-        /// Converts a target height to a jump velocity required to reach that height, considering gravity. Gravity should be negative. Returns 0 if impossible to reach height
+        /// Converts a target height to a jump velocity required to reach that height, considering gravity. Gravity should be negative if targetHeight is positive, and vice versa. Returns 0 if impossible to reach height
         /// </summary>
         public static float HeightToJumpVelocity(float targetHeight, float gravity) => -gravity * targetHeight > 0f ? Mathf.Sqrt(2f * -gravity * targetHeight) : 0f;
     }
